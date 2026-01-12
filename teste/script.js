355cbc7fb1b99e09dd97fef3a7112291942fa8bf
Raul <raullmdsantos@gmail.com>
2026-01-12 19:26:40 -0300

    update.js

--- teste/script.js
+++ teste/script.js
@@ -1,1 +1,10 @@
-document.getElementById
+var btn = document.getElementById("btnCard");
+btn.addEventListener("click", adc);
+
+function adc() {
+  var container = document.getElementById("cards");
+  var card = document.createElement("p");
+  var total = container.querySelectorAll("p").length + 1;
+  card.innerHTML = "Card " + total;
+  container.appendChild(card);
+}
