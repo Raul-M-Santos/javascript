7f20ba0bcecb60e09efe2212b86eff208e8b97fe
Raul <raullmdsantos@gmail.com>
2026-01-12 17:07:28 -0300

    Exercicios

--- aula12ex/ex014/main.js
+++ aula12ex/ex014/main.js
@@ -1,1 +1,21 @@
-alert("Olá!");
+function carregar() {
+  var msg = document.getElementById("msg");
+  var img = document.getElementById("imagem");
+  var data = new Date();
+  var hora = data.getHours();
+  msg.innerHTML = `Agora são ${hora} horas`;
+
+  if (hora >= 0 && hora <= 12) {
+    msg.innerHTML += " Bom dia!";
+    img.src = "fotomanha.jpg";
+    document.body.style.background = "#7085fd";
+  } else if (hora >= 13 && hora <= 18) {
+    msg.innerHTML += " Boa tarde!";
+    img.src = "fototarde.jpg";
+    document.body.style.background = "#9a6c36e9";
+  } else {
+    msg.innerHTML += " Boa noite!";
+    img.src = "fotonoite.jpg";
+    document.body.style.background = "#3C2844";
+  }
+}
