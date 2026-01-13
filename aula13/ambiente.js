490f92cc9fc50bc6a7288072eae5d523a4554d85
Raul <raullmdsantos@gmail.com>
2026-01-13 18:27:25 -0300

    Testes

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+/*  
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+*/
+var hora = 1
+
+while (hora <= 12) {
+    console.log (`horas ${hora}`);
+    hora++
+}
+
+for (c = 1; c <= 12; c++) {
+    console.log(`repetições ${c}`)
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,7 @@
+console.log(`Começando`)
+
+for (y = 0; y <= 10; y++) {
+  console.log(`Execução ${y}`);
+}
+
+console.log(`Terminando`)
