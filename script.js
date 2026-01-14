23a85c216051bd39b07a6347dc39c3ef1c53ae97
Raul <raullmdsantos@gmail.com>
2026-01-14 09:31:45 -0300

    testes

--- script.js
+++
@@ -1,9 +0,0 @@
-var botao = document.getElementById("botao");
-botao.addEventListener("click", txt);
-
-function txt() {
-  var container = document.getElementById("container");
-  var texto = document.createElement("p");
-  texto.innerHTML = "Texto adicionado usando JS";
-  container.appendChild(texto);
-}
