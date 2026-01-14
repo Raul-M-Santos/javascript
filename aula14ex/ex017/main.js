0bab0c3e38a4e0324cea3963892d2a09a7abb0c6
Raul <raullmdsantos@gmail.com>
2026-01-14 16:41:55 -0300

    Resolução ex02

--- aula14ex/ex017/main.js
+++ aula14ex/ex017/main.js
@@ -1,1 +1,20 @@
-alert("Olá!");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", taboada);
+
+function taboada() {
+  var num = document.getElementById("num").value;
+  var res = document.getElementById("res");
+  res.innerHTML = "";
+
+  if (num.length == 0) {
+    alert("Digite um numero");
+  }
+
+  n1 = Number(num);
+
+  for (n = 1; n <= 10; n++) {
+    var option = document.createElement("option");
+    res.appendChild(option);
+    option.innerHTML += `${n1} x ${n} = ` + n * n1;
+  }
+}
